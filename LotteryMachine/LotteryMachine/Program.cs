c8c824e8b8c653295e96f5b10d859582b0a4bf6c
Jenny.Kuo <jenny.kuo@grandsys.com>
2016-12-22 09:44:09 +0800

    Lottery Machine

---
+++ LotteryMachine/LotteryMachine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LotteryMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var numbers in LotteryMachine.Shuttler())
+            {
+                object[] f = new object[]
+         {
+                new TextBox(),
+                new Button()
+         };
+              
+                var v3 = f[0] as Button;
+                v3 = v3;
+                v3 = f[1] as Button;
+                v3 = v3;
+
+                Console.Write(numbers + " ");
+            }
+        }
+    }
+}
---
+++ LotteryMachine/LotteryMachine/Shuttler.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace LotteryMachine
+{
+    class LotteryMachine
+    {
+        private static int _maxAmountOfBalls = 100000;
+
+
+        public static void ProduceIEDic(int h )
+        {
+            object[] f = new object[]
+            {
+                new TextBox(),
+                new Button()
+            };
+
+            object k = "kkk";
+            string g = "kkk";
+
+            var n = "k";
+       
+
+        }
+
+        // 洗球
+        public static int[] Shuttler()
+        {
+            int[] list = new int[GetMaxNumber()];
+            
+            for (int a = 0; a < GetMaxNumber(); a++)
+            {
+                list[a] = a + 1; // 將值放到陣列中
+            }
+            
+            for (int a = 0; a < GetMaxNumber(); a++)
+            {
+                
+
+                int num = GetOneNumber(GetMaxNumber()); // 取要作為交換值的數字
+                // 交換: A -> C, B -> A, C -> B
+                int temp = list[a]; // 設定一暫時的變數存取要被交換的值
+                list[a] = list[num];
+                list[num] = temp;
+            }
+
+            return list;
+        }
+
+        // 取得最大值
+        public static int GetMaxNumber()
+        {
+            return _maxAmountOfBalls;
+        }
+
+        // 亂數取一個數字 回傳給呼叫的程式
+        public static int GetOneNumber(int number)
+        {
+            var rnd = new Random(Guid.NewGuid().GetHashCode());
+            int i = rnd.Next(1, number);
+            
+            return i;
+        }
+    }
+}
